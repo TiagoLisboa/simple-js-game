70a8de45dcb3e27f93f5310ca0f5341de230c9fb
Tiago Lisboa <tiago.caio.ol@gmail.com>
2017-04-10 16:00:14 +0000

    enemy random moving

--- js/main.js
+++ js/main.js
@@ -7,11 +7,13 @@ var myInterval = setInterval(tick, 0);
 
 var game_objects = []
 
-var enemy = {
+var enemy_rand = {
 	x: 600,
 	y: 200,
 	w: 32,
 	h: 32,
+	v: 200,
+	lm: {y: 0, x: 1},
 	actions: [
 		{y: 0, x: -1},
 		{y: 0, x: 1},
@@ -21,14 +23,26 @@ var enemy = {
 	move: function (y, x, dt) {
 		var nx = this.x + x * this.v * dt
 		var ny = this.y + y * this.v * dt
-		nx = nx > map.w - this.w ? map.w - this.w : nx < 0 ? 0 : nx
-		ny = ny > map.h - this.h ? map.h - this.h : ny < 0 ? 0 : ny
-		if (map.blocks[map.grid[Math.floor(ny/this.h)][Math.floor(nx/this.w)]].walk &&
+		var tile = [
+			{x: Math.floor(nx/this.w), y: Math.floor( ny/this.h)},
+			{x: Math.floor((nx+this.w)/this.w), y: Math.floor(ny/this.h)},
+			{x: Math.floor(nx/this.w), y: Math.floor((ny+this.h)/this.h)},
+			{x: Math.floor((nx+this.w)/this.w), y: Math.floor((ny+this.h)/this.h)}
+		]
+		var valid = true
+		tile.forEach(function (e, i) {
+			if(e.x < 0 || e.y < 0 || e.x > map.w/map.bw - 1 || e.y > map.h/map.bh - 1){
+				valid = false
+			}
+		})
+		if (valid && map.blocks[map.grid[Math.floor(ny/this.h)][Math.floor(nx/this.w)]].walk &&
 		map.blocks[map.grid[Math.floor((ny+this.h)/this.h)][Math.floor(nx/this.w)]].walk &&
 		map.blocks[map.grid[Math.floor(ny/this.h)][Math.floor((nx+this.w)/this.w)]].walk &&
 		map.blocks[map.grid[Math.floor((ny+this.h)/this.h)][Math.floor((nx+this.w)/this.w)]].walk) {
 			this.x = nx
 			this.y = ny
+		}else{
+			this.change_dir()
 		}
 	},
 	draw: function () {
@@ -37,10 +51,35 @@ var enemy = {
 	},
 	update: function (dt) {
 		var vm = this
-		var rand = Math.floor(Math.random() * (4))
+		var dec = Math.floor(Math.random() * 1000)
+		if (dec > 998) {
+			vm.change_dir()
+		}
+		vm.move(vm.lm.y, vm.lm.x, dt)
+	},
+	change_dir: function () {
+		var vm = this
+		var rand = Math.floor(Math.random() * (3))
 		var action = this.actions[rand]
-		console.log(action.x, action.y, dt)
-		vm.move(action.y, action.x, dt)
+		if (vm.lm.x == action.x && vm.lm.y == action.y) {
+			rand = rand == 3 ? rand - 1 : rand + 1
+			action = this.actions[rand]
+		}
+		console.log('rand', rand)
+		vm.lm = action
+		console.log(vm.lm)
+	}
+}
+
+var enemy_pf = {
+	x: 600,
+	y: 200,
+	w: 32,
+	h: 32,
+	v: 200,
+	draw: function () {},
+	update: function (dt) {
+
 	}
 }
 
@@ -74,8 +113,57 @@ var map = {
 		})
 	},
 	update: function () {
+	},
+	pathfinding: function (i, f) {
+		var vm = this
+		var queue = [{y: i.y, x: i.x, i: 0}]
+		var end = false
+		var idx = 0
+
+		var grid = [[]]
+
+		for (var i = 0; i < vm.grid.length; i++) {
+			grid[i] = []
+			for (var j = 0; j < vm.grid[i].length; j++) {
+				grid[i][j] = vm.grid[i][j]
+			}
+		}
+
+		grid[f.y][f.x] = 'f'
+
+		console.log(grid)
+
+		while (!end) {
+			idx += 1
+			queue.forEach(function (qe, qi) {
+				var sur = [
+					{y: qe.y+1, x: qe.x, i: idx},
+					{y: qe.y, x: qe.x+1, i: idx},
+					{y: qe.y-1, x: qe.x, i: idx},
+					{y: qe.y, x: qe.x-1, i: idx},
+				]
+				sur.forEach(function (e, i) {
+					var similar = queue.filter(function (el, id) {
+						return el.x == e.x && el.y == e.y
+					})
+					console.log(similar.length)
+					
+					if (grid[e.y] && grid[e.y][e.x] && grid[e.y][e.x] == 0 && similar.length == 0) {
+						queue.push(e)
+						grid[e.y][e.x] = String(e.i)
+					}
+					if (grid[e.y][e.x] == 'f') {
+						end = true
+					}
+				})
+			})
+		}
+
+		console.log(queue)
 	}
 }
+map.pathfinding({x: 5, y: 7},  {x: 7, y: 8})
+
 var player = {
 	h: 32,
 	w: 32,
@@ -115,13 +203,26 @@ var player = {
 		var ny = this.y + y * this.v * dt
 		nx = nx > map.w - this.w ? map.w - this.w : nx < 0 ? 0 : nx
 		ny = ny > map.h - this.h ? map.h - this.h : ny < 0 ? 0 : ny
-		if (map.blocks[map.grid[Math.floor(ny/this.h)][Math.floor(nx/this.w)]].walk &&
+		var tile = [
+			{x: Math.floor(nx/this.w), y: Math.floor( ny/this.h)},
+			{x: Math.floor((nx+this.w)/this.w), y: Math.floor(ny/this.h)},
+			{x: Math.floor(nx/this.w), y: Math.floor((ny+this.h)/this.h)},
+			{x: Math.floor((nx+this.w)/this.w), y: Math.floor((ny+this.h)/this.h)}
+		]
+		var valid = true
+		tile.forEach(function (e, i) {
+			if(e.x < 0 || e.y < 0 || e.x > map.w/map.bw - 1 || e.y > map.h/map.bh - 1){
+				valid = false
+				console.log(e.x, e.y)
+			}
+		})
+		if (valid && map.blocks[map.grid[Math.floor(ny/this.h)][Math.floor(nx/this.w)]].walk &&
 		map.blocks[map.grid[Math.floor((ny+this.h)/this.h)][Math.floor(nx/this.w)]].walk &&
 		map.blocks[map.grid[Math.floor(ny/this.h)][Math.floor((nx+this.w)/this.w)]].walk &&
 		map.blocks[map.grid[Math.floor((ny+this.h)/this.h)][Math.floor((nx+this.w)/this.w)]].walk) {
 			this.x = nx
 			this.y = ny
-		}
+		}	
 	},
 	update: function (dt) {
 		var vm = this
@@ -136,7 +237,6 @@ var player = {
 player.listeners();
 
 game_objects.push(map)
-game_objects.push(enemy)
 game_objects.push(player)
 
 function tick() {
